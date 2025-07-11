758ec83695bf08753be8de3b3ffe8a14913d97e4
Gianluca <gbelardelli@gmail.com>
2025-07-11 17:15:32 +0200

    handlers: implemented characters CRUD

--- src/handlers/character_handler.rs
+++ src/handlers/character_handler.rs
@@ -1,7 +1,7 @@
 use actix_web::{web, HttpResponse, Responder};
 use sqlx::SqlitePool;
 
-use crate::{db::character_db, models::character_model::NewCharacter};
+use crate::{db::character_db, error::AppError, models::character_model::{CharacterModel, NewCharacter}};
 
 pub fn config(cfg: &mut web::ServiceConfig) {
     cfg
@@ -22,7 +22,34 @@ async fn create_character(db_pool: web::Data<SqlitePool>, new_character: web::Js
     }
 }
 
-async fn get_all_characters() -> impl Responder { HttpResponse::Ok().body("get_all_characters") }
-async fn get_character_by_id() -> impl Responder { HttpResponse::Ok().body("get_character_by_id") }
-async fn update_character() -> impl Responder { HttpResponse::Ok().body("update_character") }
-async fn delete_character() -> impl Responder { HttpResponse::Ok().body("delete_character") }
+async fn get_all_characters(db_pool: web::Data<SqlitePool>,) -> Result<HttpResponse, AppError> {
+    let characters = character_db::get_all_characters(db_pool.get_ref()).await
+        .map_err(|e| AppError::DbError(e))?;
+
+    Ok(HttpResponse::Ok().json(characters))
+}
+
+async fn get_character_by_id(db_pool: web::Data<SqlitePool>, path: web::Path<i64>,) -> Result<HttpResponse, AppError> {
+    let id = path.into_inner();
+    match character_db::get_character_by_id(&db_pool, id).await {
+        Ok(Some(session)) => Ok(HttpResponse::Ok().json(session)),
+        Ok(None) => Err(AppError::NotFound(format!("Character with id {} not found", id))),
+        Err(e) => Err(AppError::DbError(e)),
+    }
+}
+
+async fn update_character(db_pool: web::Data<SqlitePool>, path: web::Path<i64>, character: web::Json<CharacterModel>,) -> Result<HttpResponse, AppError> {
+    let id = path.into_inner();
+    match character_db::update_character(&db_pool, id, character.into_inner()).await {
+        Ok(()) => Ok(HttpResponse::Ok().json(id)),
+        Err(e) => Err(AppError::DbError(e)),
+    }
+}
+
+async fn delete_character(db_pool: web::Data<SqlitePool>, path: web::Path<i64>,) -> Result<HttpResponse, AppError> {
+    let id = path.into_inner();
+    match character_db::delete_character(&db_pool, id).await {
+        Ok(()) => Ok(HttpResponse::Ok().json(id)),
+        Err(e) => Err(AppError::DbError(e)),
+    }
+}
